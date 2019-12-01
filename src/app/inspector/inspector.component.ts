4a5adfa5fb2661aba285623232f8c91ef395ac69
Fanaen <contact@fanaen.fr>
2019-12-01 12:51:11 +0100

    feat(inspector): add systems' details
    Display root systems as default content and display system's children.

--- src/app/inspector/inspector.component.ts
+++ src/app/inspector/inspector.component.ts
@@ -15,8 +15,13 @@ export class InspectorComponent implements OnInit {
         systems: []
     };
 
-    private selectedSystem: System;
-    private selectedSubsystem: Subsystem;
+    // -- To diplay --
+    // 1. for the current system
+    public selectedSystem: System;
+    public selectedSystemSystems: System[];
+    public selectedSystemSubsystems: System[];
+    // 2. for the current subsystem
+    public selectedSubsystem: Subsystem;
 
     constructor(private graphUpdater: GraphUpdaterService, private selector: SelectorService) {
     }
@@ -32,11 +37,16 @@ export class InspectorComponent implements OnInit {
                 // Analyse the selection
                 switch (selected.type) {
                     case ObjectType.System:
-                        this.selectedSystem = this.graph.systems.find(s => s.id == selected.id);
+                        const { system, index } =  this.findSystem(selected.id);
+                        this.selectedSystem = system;
+                        this.selectedSystemSystems = this.getSystemsWithParent(index);
+                        this.selectedSystemSubsystems = this.getSubsystemsWithParent(index);
                         this.selectedSubsystem = null;
                         break;
                     case ObjectType.Subsystem:
                         this.selectedSystem = null;
+                        this.selectedSystemSystems = null;
+                        this.selectedSystemSubsystems = null;
                         this.selectedSubsystem = this.graph.subsystems.find(s => s.id == selected.id);
                         break;
                 }
@@ -54,5 +64,33 @@ export class InspectorComponent implements OnInit {
 
     getSubsystem(subsystem: ReferenceByIndex<Subsystem>): Subsystem {
         return this.graph.subsystems[subsystem.index];
+    }
+
+    getSystemsWithParent(index: number | undefined): System[] {
+        if (typeof index == 'number') {
+            return this.graph.systems.filter(s => s.parent_system && s.parent_system.index == index);
+        }
+        else {
+            return this.graph.systems.filter(s => !s.parent_system);
+        }
+    }
+
+    getSubsystemsWithParent(index: number | undefined): System[] {
+        if (typeof index == 'number') {
+            return this.graph.subsystems.filter(s => s.parent_system && s.parent_system.index == index);
+        }
+        else {
+            return this.graph.subsystems.filter(s => !s.parent_system);
+        }
+    }
+
+    private findSystem(id: string) {
+        const list =  this.graph.systems;
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].id == id) {
+                return { system: list[i], index: i };
+            }
+        }
+        return { system: null, index: -1 };
     }
 }
